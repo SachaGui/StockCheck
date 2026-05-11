2229b8e715476b153b847220f976ada4595e3804
KeiShocho <keishocho@outlook.com>
2026-05-11 17:16:26 +0900

    feat: 初始化Spring Boot骨架

---
+++ src/test/java/com/ala/stockcheck/StockCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ala.stockcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
