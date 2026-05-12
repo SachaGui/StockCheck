dd02103e53625648ebe4f4ecd22ba9deab5f3ca9
Raynor-zeng <peterzeng001@gmail.com>
2026-05-12 19:05:34 +0900

    创建盘点任务功能上传

--- src/main/java/com/ala/stockcheck/controller/StockCheckController.java
+++ src/main/java/com/ala/stockcheck/controller/StockCheckController.java
@@ -1,5 +1,35 @@
 package com.ala.stockcheck.controller;
 
+import com.ala.stockcheck.common.Result;
+import com.ala.stockcheck.entity.StockCheck;
+import com.ala.stockcheck.service.StockCheckService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 棚卸タスクController
+ */
+@RestController
+@RequestMapping("/api/checks")
 public class StockCheckController {
 
+    private final StockCheckService stockCheckService;
+
+    public StockCheckController(StockCheckService stockCheckService) {
+        this.stockCheckService = stockCheckService;
+    }
+
+    /**
+     * 棚卸タスク作成
+     *
+     * @return 作成結果
+     */
+    @PostMapping
+    public Result<StockCheck> createCheck() {
+
+        StockCheck stockCheck = stockCheckService.createCheck();
+
+        return Result.success(stockCheck);
+    }
 }
--- src/test/java/com/ala/stockcheck/StockCheckApplicationTests.java
+++ src/test/java/com/ala/stockcheck/StockCheckApplicationTests.java
@@ -1,13 +1,32 @@
 package com.ala.stockcheck;
 
+import com.ala.stockcheck.entity.StockCheck;
+import com.ala.stockcheck.service.StockCheckService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// テストクラス
 @SpringBootTest
-class StockCheckApplicationTests {
+public class StockCheckApplicationTests {
+
+	// 棚卸タスク
+	@Autowired
+	private StockCheckService stockCheckService;
+
+	// 棚卸タスク作成テスト
 
 	@Test
-	void contextLoads() {
-	}
+	void testCreateCheck() {
 
+		StockCheck stockCheck = stockCheckService.createCheck();
+
+		System.out.println("===== テスト結果 =====");
+
+		System.out.println("ID : " + stockCheck.getId());
+
+		System.out.println("Status : " + stockCheck.getStatus());
+
+		System.out.println("CreateTime : " + stockCheck.getCreateTime());
+	}
 }
