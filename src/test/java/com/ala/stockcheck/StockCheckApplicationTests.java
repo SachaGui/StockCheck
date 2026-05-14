4578a0eb1c31cba33a305fa9f90e4d9d56cb7d86
Raynor-zeng <peterzeng001@gmail.com>
2026-05-14 10:31:01 +0900

    去除创建盘点任务时的ID获取，简化代码

--- src/test/java/com/ala/stockcheck/StockCheckApplicationTests.java
+++ src/test/java/com/ala/stockcheck/StockCheckApplicationTests.java
@@ -1,32 +1,30 @@
-package com.ala.stockcheck;
-
-import com.ala.stockcheck.entity.StockCheck;
-import com.ala.stockcheck.service.StockCheckService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-// テストクラス
-@SpringBootTest
-public class StockCheckApplicationTests {
-
-	// 棚卸タスク
-	@Autowired
-	private StockCheckService stockCheckService;
-
-	// 棚卸タスク作成テスト
-
-	@Test
-	void testCreateCheck() {
-
-		StockCheck stockCheck = stockCheckService.createCheck();
-
-		System.out.println("===== テスト結果 =====");
-
-		System.out.println("ID : " + stockCheck.getId());
-
-		System.out.println("Status : " + stockCheck.getStatus());
-
-		System.out.println("CreateTime : " + stockCheck.getCreateTime());
+	package com.ala.stockcheck;
+	
+	import com.ala.stockcheck.entity.StockCheck;
+	import com.ala.stockcheck.service.StockCheckService;
+	import org.junit.jupiter.api.Test;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.boot.test.context.SpringBootTest;
+	
+	// テストクラス
+	@SpringBootTest
+	public class StockCheckApplicationTests {
+	
+		// 棚卸タスク
+		@Autowired
+		private StockCheckService stockCheckService;
+	
+		// 棚卸タスク作成テスト
+	
+		@Test
+		void testCreateCheck() {
+	
+			StockCheck stockCheck = stockCheckService.createCheck();
+	
+			System.out.println("===== テスト結果 =====");
+	
+			System.out.println("Status : " + stockCheck.getStatus());
+	
+			System.out.println("CreateTime : " + stockCheck.getCreateTime());
+		}
 	}
-}
